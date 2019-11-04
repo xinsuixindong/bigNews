6e7ab087249cfe4e87c8ac25815c13004205fc6d
wangjl <2453852787@qq.com>
2019-11-04 19:01:30 +0800

    评论渲染完成

---
+++ js/commit_list.js
@@ -0,0 +1,10 @@
+$.ajax({
+    type:'get',
+    url:'http://47.111.184.55:8888/api/v1/admin/comment/search',
+    success:function(data){  
+        console.log(data);
+        var html = template('commentTpl', {data : data});
+        console.log(html);  
+		$('#commentBox').html(html);
+    }
+})
